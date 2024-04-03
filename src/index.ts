475381a7bf58ad88dc45f3529e04ff9c325a2b1e
小猪乱跑 <xiaozhulianpao@gmail.com>
2024-04-03 22:35:12 +0800

    feat: 增加模版渲染

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,46 @@
+import ejs from 'ejs'
 import fse from 'fs-extra'
+import { glob } from 'glob'
 import path from "node:path"
+import ora from 'ora'
 
 export default async function install() {
-    console.log('install exec', JSON.parse(process.env.CLI_PKG_INFO || ''), JSON.parse(process.env.CLI_TPL_INFO || ''), JSON.parse(process.env.CLI_PROJECT_INFO || ''))
     const { cacheFilePath } = JSON.parse(process.env.CLI_PKG_INFO || '')
     const root = cacheFilePath
-    // const cloneSpinner = ora(`安装${this.tpl.name}模版...`).start()
-    // try {
-    const targetPath = process.cwd()
-    const srcPath = path.resolve(root, 'template')
-    fse.ensureDirSync(targetPath)
-    fse.ensureDirSync(srcPath)
-    fse.copySync(srcPath, targetPath)
-    // await this.ejsRender()
-    // cloneSpinner.succeed()
-    // if (this.tpl.scripts) {
-    //     for (let i = 0; i < this.tpl.scripts.length; i++) {
-    //         const script = this.tpl.scripts[i]
-    //         await this.execCommand(script)
-    //     }
-    // }
-    // }
-    // catch (error) {
-    //     cloneSpinner.isSpinning && cloneSpinner.stopAndPersist({ symbol: '❌' })
-    //     throw error
-    // }
+    const cloneSpinner = ora(`安装项目模版...`).start()
+    try {
+        const targetPath = process.cwd()
+        const srcPath = path.resolve(root, 'template')
+        fse.ensureDirSync(targetPath)
+        fse.ensureDirSync(srcPath)
+        fse.copySync(srcPath, targetPath)
+        await ejsRender()
+        cloneSpinner.succeed()
+    }
+    catch (error) {
+        cloneSpinner.isSpinning && cloneSpinner.stopAndPersist({ symbol: '❌' })
+        throw error
+    }
+}
+
+async function ejsRender() {
+    const projectInfo = JSON.parse(process.env.CLI_PROJECT_INFO || '')
+    const dir = process.cwd()
+    const files = await glob('**', {
+        cwd: dir,
+        nodir: true,
+        ignore: ['**/node_modules/**'],
+    })
+    return Promise.all(files.map(async (file) => {
+        const filePath = path.resolve(dir, file)
+        return new Promise((resolve, reject) => {
+            ejs.renderFile(filePath, projectInfo, {}, async (error, content) => {
+                if (error)
+                    return reject(error)
+
+                fse.writeFileSync(filePath, content, 'utf-8')
+                resolve(content)
+            })
+        })
+    }))
 }
