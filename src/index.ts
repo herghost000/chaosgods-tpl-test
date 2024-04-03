00807a90be9ffc36b2bfe395ab1d9d4a2bf3e852
小猪乱跑 <xiaozhulianpao@gmail.com>
2024-04-03 22:21:23 +0800

    fix: 拷贝模版到目录

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import path from "node:path"
 
 export default async function install() {
     console.log('install exec', JSON.parse(process.env.CLI_PKG_INFO || ''), JSON.parse(process.env.CLI_TPL_INFO || ''), JSON.parse(process.env.CLI_PROJECT_INFO || ''))
+    const { cacheFilePath } = JSON.parse(process.env.CLI_PKG_INFO || '')
+    const root = cacheFilePath
     // const cloneSpinner = ora(`安装${this.tpl.name}模版...`).start()
     // try {
     const targetPath = process.cwd()
-    const srcPath = path.resolve(__dirname, '../template')
+    const srcPath = path.resolve(root, 'template')
     fse.ensureDirSync(targetPath)
     fse.ensureDirSync(srcPath)
-    console.log(srcPath, targetPath)
     fse.copySync(srcPath, targetPath)
     // await this.ejsRender()
     // cloneSpinner.succeed()
